eba1a599ae0d6266611bfaee11067c95ba618d3b
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2019-11-20 15:51:58 -0700

    Implementation of SpecificObjectsCollection in progress (Adding Unit Tests)...

---
+++ src/exceptions/SpecifiedClassNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+namespace VersatileCollections\Exceptions;
+
+/**
+ * Description of NonExistentItemException
+ *
+ * @author Rotimi Ade
+ */
+class SpecifiedClassNotFoundException extends \Exception {}
