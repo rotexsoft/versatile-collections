f3ca5be916b71a0263ea12e541a24d3f1411ac56
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2020-12-05 03:23:17 -0700

    Updated rector requirement and renabled it for travis

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+    // Define what rule sets will be applied
+    $parameters->set(Option::SETS, [
+        SetList::DEAD_CODE,
+        SetList::PHP_72,
+        SetList::PHP_73,
+        //SetList::PHP_74,
+        //SetList::PHP_80,
+        SetList::PERFORMANCE,
+    ]);
+
+    // get services (needed for register a single rule)
+    // $services = $containerConfigurator->services();
+
+    // register a single rule
+    // $services->set(TypedPropertyRector::class);
+};
